71403d2fe59a4693050a333bc350e7c8ca9b3d23
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-05-11 17:21:06 +0300

    Fix test

--- packages/graphql-codegen-cli/tests/__mocks__/some-fetch.js
+++ packages/graphql-codegen-cli/tests/__mocks__/some-fetch.js
@@ -7,6 +7,9 @@ module.exports = {
     const schemaFile = readFileSync(join(__dirname, '../test-documents/schema.graphql'), 'utf8');
     const schema = buildSchema(schemaFile);
     return {
+      headers: {
+        'content-type': 'application/json'
+      },
       json() {
         global.CUSTOM_FETCH_FN_CALLED = true;
         return graphql(schema, introspectionQuery);
