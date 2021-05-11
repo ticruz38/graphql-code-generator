936876328a2bf7245572283731f6fb929ab4c61d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-05-11 17:31:20 +0300

    Update examples

--- packages/graphql-codegen-cli/tests/__mocks__/some-fetch.js
+++ packages/graphql-codegen-cli/tests/__mocks__/some-fetch.js
@@ -1,18 +1,21 @@
-const { readFileSync } = require('fs');
+const { promises: { readFile } } = require('fs');
 const { join } = require('path');
 const { buildSchema, graphql, introspectionQuery } = require('graphql');
 
 module.exports = {
-  someFetchFn: async (url, options) => {
-    const schemaFile = readFileSync(join(__dirname, '../test-documents/schema.graphql'), 'utf8');
+  someFetchFn: async () => {
+    const schemaFile = await readFile(join(__dirname, '../test-documents/schema.graphql'), 'utf8');
     const schema = buildSchema(schemaFile);
+    global.CUSTOM_FETCH_FN_CALLED = true;
     return {
       headers: {
         'content-type': 'application/json'
       },
-      json() {
-        global.CUSTOM_FETCH_FN_CALLED = true;
-        return graphql(schema, introspectionQuery);
+      async text() {
+        return JSON.stringify(await graphql(schema, introspectionQuery));
+      },
+      async json() {
+        return JSON.parse(await this.text());
       },
     };
   },
